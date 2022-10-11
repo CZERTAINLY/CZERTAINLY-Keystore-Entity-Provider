fb9ca75f5379101390410f4a35d785257f55067b
3KeyPradeep <pradeep.saminathan@3key.company>
2022-10-11 15:39:00 +0100

    Update entity renewal and cleanup after issue failure
    This PR contains the following changes.
    
    1. Include meta data to generate CSR method to include the alias name
    2. Include the logic to check for renewal request before generating the key pair

--- src/main/java/com/czertainly/provider/entity/keystore/service/LocationService.java
+++ src/main/java/com/czertainly/provider/entity/keystore/service/LocationService.java
@@ -2,14 +2,7 @@ package com.czertainly.provider.entity.keystore.service;
 
 import com.czertainly.api.exception.LocationException;
 import com.czertainly.api.exception.NotFoundException;
-import com.czertainly.api.model.connector.entity.GenerateCsrRequestDto;
-import com.czertainly.api.model.connector.entity.GenerateCsrResponseDto;
-import com.czertainly.api.model.connector.entity.LocationDetailRequestDto;
-import com.czertainly.api.model.connector.entity.LocationDetailResponseDto;
-import com.czertainly.api.model.connector.entity.PushCertificateRequestDto;
-import com.czertainly.api.model.connector.entity.PushCertificateResponseDto;
-import com.czertainly.api.model.connector.entity.RemoveCertificateRequestDto;
-import com.czertainly.api.model.connector.entity.RemoveCertificateResponseDto;
+import com.czertainly.api.model.connector.entity.*;
 
 public interface LocationService {
 
